d9e717a9969e7e9e5c59b2c07fb3e9a630d29dc3
Matheus Otto <12745@proway.treina>
2026-04-08 21:47:55 -0300

    Aula de operadores logicos

--- assets/js/logica-03-if.js
+++ assets/js/logica-03-if.js
@@ -91,3 +91,90 @@ function avaliacaoFinalizar(){
     bom = 0
     otimo = 0
 }
+
+function exemploOperadorE() {
+    let idade = parseInt(prompt("Digite a idade: "))
+  
+    // 0, 1, 2, 3, 4... 17
+
+    if ((idade >= 0) && (idade <= 17)) {
+      alert("Criança ou adolescente")
+    } else if (idade >= 18) {
+      alert("adulto")
+    } else {
+      alert("idade inválida")
+    }
+  }
+
+function  exemploOperadorOu(){
+    let transport = prompt("digite o meio para viajar");
+
+    if ((transport === "moto") || (transport === "carro")){
+        alert("Viajar de boas")
+    } else {
+        alert("Não vamos viajar");
+    }
+}
+
+function exemploLoja(){
+    let nome1 = prompt("Digite o nome do jogo 1");
+    let categoria1 = prompt("Digite a categoria: ");
+    let precoBase1 = 399.99;
+    
+    let precoComDesconto1 = 0;
+    let percentualDesconto1 = 0;
+    let precoDesconto1 = 0;
+
+    if ((categoria1 === "moba") || (categoria1 === "fps")){
+        percentualDesconto1 = 0.10;
+        precoDesconto1 = percentualDesconto1 * precoBase1;
+        precoComDesconto1 = precoBase1 - percentualDEsconto1;
+    } else if ((categoria1 === "aventura") || (categoria1 === "rpg")) {
+        percentualDesconto1 = 0.15;
+        precoDesconto1 = percentualDesconto1 * precoBase1;
+        precoComDesconto1 = precoBase1 - precoDesconto1; 
+    } else if ((categoria1 === "roquelike") || (categoria1 === "soulslike")){
+        percentualDesconto1 = 0.20;
+        precoDesconto1 = percentualDesconto1 * precoBase1;
+        precoComDesconto1 = precoBase1 - precoDesconto1;
+    }
+
+    let nome2 = prompt("digite o nome do jogo 2");
+    let categoria2 = prompt("Digite a categoria do jogo");
+    let precoBase2 = 190.00;
+
+    let precoComDesconto2 = 0;
+    let percentualDesconto2 = 0;
+    let precoDesconto2 = 0;
+
+    if ((categoria2 === "moba") || (categoria2 === "fps")){
+        percentualDesconto2 = 0.10;
+        precoDesconto2 = percentualDesconto2 * precoBase2;
+        precoComDesconto2 = precoBase2 - precoDesconto2; 
+    } else if ((categoria2 === "aventura")||(categoria2 === "rpg")){
+        percentualDesconto2 = 0.15;
+        precoDesconto2 = percentualDesconto2 * precoBase2;
+        precoComDesconto2 = precoBase2 - precoDesconto2;
+    } else if ((categoria2 === "roquelike") || (categoria2 === "soulslike")){
+        percentualDesconto2 = 0.20;
+        precoDesconto2 = percentualDesconto2 * precoBase2;
+        precoComDesconto2 = precoBase2 - precoDesconto2;
+    }
+
+    alert(
+        "Nome: " + nome1 +
+        "\nCategoria: " + categoria1 +
+        "\nPreço: R$" + precoBase1 +
+        "\nDesconto: " + percentualDesconto1 * 100 + "%" +
+        "\nPreço com desconto: R$" + precoComDesconto1.toFixed(2) +
+
+        "\n" + 
+        "\nNome: " + nome2 +
+        "\nCategoria: " + categoria2 +
+        "\nPreço: R$" + precoBase2 + 
+        "\nDesconto: " + percentualDesconto2 * 100 + "%" +
+        "\nPreço co desconto: R$" + precoComDesconto2.toFixed(2)
+    )
+}
+
+

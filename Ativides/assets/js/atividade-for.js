b8eda6ae9492f7b16958d1005bd50d7eaaec0e68
Matheus Otto <12745@proway.treina>
2026-04-20 21:50:29 -0300

    Atividades de for

--- Ativides/assets/js/atividade-for.js
+++ Ativides/assets/js/atividade-for.js
@@ -35,72 +35,72 @@ function exercicio05MostrarNumeros() {
 function exercicio06MostrarNumerosPares() {
     let quantidadePares = 0;
     for (let indice = 0; indice <= 10; indice = indice + 1) {
-        if (indice % 2 === 0){
+        if (indice % 2 === 0) {
             console.log("Numero par: " + indice);
             quantidadePares = quantidadePares + 1;
         }
     }
 }
 
-function exercicio07MostrarNumerosImpares(){
-    for (let indice = 0; indice <= 9; indice = indice + 1){
-        if (indice % 2 !== 0){
+function exercicio07MostrarNumerosImpares() {
+    for (let indice = 0; indice <= 9; indice = indice + 1) {
+        if (indice % 2 !== 0) {
             console.Log("Numeros Impar: " + indice);
         }
     }
 }
 
-function exercicio08SolicitarTresNomes(){
-    for (let indice = 0; indice < 3; indice = indice + 1){
-        let nome  = prompt("Digite um nome: ");
+function exercicio08SolicitarTresNomes() {
+    for (let indice = 0; indice < 3; indice = indice + 1) {
+        let nome = prompt("Digite um nome: ");
         console.log(nome);
     }
 }
 
-function exercicio09SomarTresIdades(){
+function exercicio09SomarTresIdades() {
     let somaIdades = 0;
-    for (let indice = 0; indice < 3; indice = indice + 1){
+    for (let indice = 0; indice < 3; indice = indice + 1) {
         let idade = parseInt(prompt("Digite uma idade: "));
         somaIdades = somaIdades + idade;
     }
     console.log(somaIdades);
 }
 
-function exercicio10CalcularMediaSimples(){
+function exercicio10CalcularMediaSimples() {
     let mediaNotas = 0;
-    for (let indice = 0; indice < 4; indice = indice + 1){
+    for (let indice = 0; indice < 4; indice = indice + 1) {
         let nota = parseInt(prompt("Digite a nota: "));
         mediaNotas = mediaNotas + nota;
     }
-    mediaNotas = mediaNotas/4;
+    mediaNotas = mediaNotas / 4;
     console.log(mediaNotas);
 }
 
-function exercicio11ContarMaioresDeIdade(){
+function exercicio11ContarMaioresDeIdade() {
     let quantidadeMaiorDezoito = 0;
-    for (let indice = 0; indice > 5; indice = indice + 1){
+    for (let indice = 0; indice > 5; indice = indice + 1) {
         let idade = parseInt(prompt("Digite a idade: "));
-        if (idade >= 18){
+        if (idade >= 18) {
             quantidadeMaiorDezoito = quantidadeMaiorDezoito + 1
         }
     }
     console.log("Pessoas maiores de dezoito ou igual: " + quantidadeMaiorDezoito);
 }
 
-function exercicio12MostrarTabuada(){
+function exercicio12MostrarTabuada() {
     let numero = parseInt(prompt("Digite o numero: "));
-    for (let indice = 0; indice < 11; indice = indice + 1){
+    for (let indice = 0; indice < 11; indice = indice + 1) {
         let multiplicacao = numero * indice;
-        console.log( indice + " x " + numero + "= " + multiplicacao);
+        console.log(indice + " x " + numero + "= " + multiplicacao);
     }
 }
 
-function exexercicio13ContarLetrasNomes(){
-    for (let indice = 0; indice < 4; indice = indice + 1){
+function exexercicio13ContarLetrasNomes() {
+    for (let indice = 0; indice < 4; indice = indice + 1) {
         let nome = prompt("Digite um nome: ");
         console.log("Nome: " + nome + "Quantidade de letras: " + nome.length)
     }
 }
 
-function 
+function
 
